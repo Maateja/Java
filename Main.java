9e09c3d8b076cf164f1cb38620529253a3af6ae0
Maateja <naikmteja@gmail.com>
2026-08-18 14:25:49 +0530

    java class codes

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 // FOR LOOP
 
-// public class Main {
-//     public static void main(String[] args) {
-//         for (int i = 1; i <= 5; i++) {
-//             System.out.println(i);
-//         }
-//     }
-// }
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
 
 // WHILE LOOP
 
